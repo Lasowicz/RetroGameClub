7c1ea1e895ec7d43d309af8878528000ff03b240
Wsewolod Lasowicz <vsevolodlyasovich@yandex.ru>
2026-03-22 16:32:14 +0500

    1. Created the background. 2. Added some more content. 3. Animated the text.

--- JS/app.js
+++ JS/app.js
@@ -10,6 +10,14 @@ const imagesGameIcons = [
     '/RetroGameClub/images/game_icons/oblivion.jpg',
     '/RetroGameClub/images/game_icons/warcraft3.jpeg',
     '/RetroGameClub/images/game_icons/willrock.jpg',
+    '/RetroGameClub/images/game_icons/wc3rc.webp',
+    '/RetroGameClub/images/game_icons/sacred.webp',
+    '/RetroGameClub/images/game_icons/gothic3.jpg',
+    '/RetroGameClub/images/game_icons/cod1.jpg',
+    '/RetroGameClub/images/game_icons/maxpayne1.webp',
+    '/RetroGameClub/images/game_icons/morrowind.webp',
+    '/RetroGameClub/images/game_icons/RomeTW1.jpg',
+    '/RetroGameClub/images/game_icons/cossacks.webp',
 ]
 
 imagesGameIcons.forEach(src => {
@@ -17,3 +25,29 @@ imagesGameIcons.forEach(src => {
     img.src = src;
     gallery.appendChild(img);
 })
+
+gallery.addEventListener('click', () => {
+    alert('clicked');
+})
+
+                                 /*Анимация для BEST GAMES FROM THE WORLD!*/
+
+function changeText() {
+    const bestGameText = document.getElementById('bestGamesText');
+    const bestGameTextChange = bestGameText.textContent;
+    bestGameText.textContent = '';
+    for (let i = 0; i <= bestGameTextChange.length; i++) {
+        setTimeout(() => {
+            if (i === 0) {
+                bestGameText.textContent = ' ';
+            } else {
+                bestGameText.textContent = bestGameTextChange.slice(0, i);
+            }
+            if (i === bestGameTextChange.length) {
+                setTimeout(changeText, 100);
+            }
+        }, i * 100);
+    }
+}
+
+changeText();
